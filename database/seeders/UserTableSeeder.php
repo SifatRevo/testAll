c3a0e1e747fcf6a0817e947b5edccd9ed592e8dc
sifat404 <sifat@revo-interactive.com>
2023-03-27 02:42:05 +0600

    database setup and seeding

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if (User::count() > 0) {
+            return;
+        }
+
+       User::factory(10)->create();
+    }
+}
